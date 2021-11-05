86537061c9f6ee1e5a6c3df588b2efb70cf950ca
Kate Sky <kateskytech@gmail.com>
2021-11-05 16:41:56 -0500

    refactor example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component, ViewChild } from '@angular/core';
+import { UserService } from './user.service';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularDay.It 2021';
   filters: string[] = [];
+  date: string | null = '';
+  flag = false;
+
+  constructor(private readonly userService: UserService){
+
+  }
+  async ngOnInit(): Promise<void> {
+    this.flag = await this.userService.needsRenewFlag();
+    if(this.flag){
+      this.date = `Renew by ${this.userService.renewDate}`;
+    }
+  }
+
+
   ssnChanged(checkbox: any) {
     if (checkbox.currentTarget.checked) {
       this.filters.push('ssn');
