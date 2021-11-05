f744675ea650fb63791459506d5f5a61fc2337b0
Kate Sky <kateskytech@gmail.com>
2021-11-05 13:04:48 -0500

    example-3 only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { RenewDirective } from './renew.directive';
-import { SettingsDirective } from './settings.directive';
-
+import { AppComponent } from './app.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RenewDirective,
-    SettingsDirective,
     FiltersDirective
   ],
   imports: [
